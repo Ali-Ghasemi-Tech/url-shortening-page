4c94affad13ee764286625d6e1269e1f94ef04ba
TLB30 <thelastblade30@gmail.com>
2023-08-28 16:59:31 +0330

    setup project with tailwind and react , make navbar for desktop version

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <body className='flex justify-center pt-[60px]'>
+      <Navbar />
+    </body>
+  );
+}
+
+export default App;
